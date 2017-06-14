ccd0f940135ee0122c282516502d3c54f4617dc8
Beau Hankins <beau.j.hankins@gmail.com>
2017-06-14 19:04:06 +1000

    Add pages

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,17 +1,28 @@
 $(function() {
+  AOS.init();
+
   var $nav = $('nav');
-  var $pressSection = $('#press');
 
   var breakpoint = $nav.offset().top;
   var navHeight = $nav.innerHeight();
 
   $(window).scroll(function() {
-    if ($(window).scrollTop() < breakpoint) {
+    if ($(window).scrollTop() <= breakpoint) {
       $nav.removeClass('fixed top-0 left-0 right-0');
-      $pressSection.css('margin-top', 0);
+      $nav.next().css('margin-top', 0);
     } else {
       $nav.addClass('fixed top-0 left-0 right-0');
-      $pressSection.css('margin-top', navHeight + 'px');
+      $nav.next().css('margin-top', navHeight + 'px');
     }
+  });
+
+  $('#specs-nav').click(function() {
+    $('.specs-section').show();
+    $('.what-section').hide();
+  });
+
+  $('#what-nav').click(function() {
+    $('.what-section').show();
+    $('.specs-section').hide();
   });
 });
