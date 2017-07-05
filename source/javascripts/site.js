63a6339c7c7e3a03a18716582c5072ee682c3ddb
Applant <beau.j.hankins+applant@gmail.com>
2017-07-05 10:55:58 +1000

    Add simple steps videos, various fixes

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,6 +1,13 @@
 $(function() {
-  AOS.init();
+  initStickyNav();
+  initPressSlider();
+  bindAtreeNav();
+  bindHamburger();
 
+  AOS.init();
+});
+
+function initStickyNav() {
   var $nav = $('nav');
 
   var breakpoint = $nav.offset().top;
@@ -16,7 +23,32 @@ $(function() {
       $nav.next().css('margin-top', navHeight + 'px');
     }
   });
+}
 
+function initPressSlider() {
+  var slides = $('#press-slider > div');
+
+  slides.each(function() {
+    $(this).css({ opacity: 0 });
+  });
+
+  $(slides[0]).css({ opacity: 1 });
+
+  var i = 0;
+  setInterval(function() {
+    $(slides[i]).animate({ opacity: 0, 'margin-left': '-100%' }, 200);
+
+    i++;
+    if (i >= slides.length) {
+      i = 0;
+    }
+
+    $(slides[i]).css({ opacity: 0, 'margin-left': '100%' });
+    $(slides[i]).animate({ opacity: 1, 'margin-left': '0' }, 200);
+  }, 5000);
+}
+
+function bindAtreeNav() {
   $('#specs-nav').click(function() {
     $('.specs-section').show();
     $('.what-section').hide();
@@ -26,8 +58,10 @@ $(function() {
     $('.what-section').show();
     $('.specs-section').hide();
   });
+}
 
+function bindHamburger() {
   $('#hamburger').click(function() {
     $('#menu').toggleClass('open');
   });
-});
+}
